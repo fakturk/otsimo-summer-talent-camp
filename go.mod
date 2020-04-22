5143264a9d8864fe648243050155c4bc4612efb5
fakturk <f.akturk@gmail.com>
2020-04-23 01:06:23 +0300

    mongo db connection made and getCandidates() implemented

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/fakturk/otsimo-summer-talent-camp
 
 go 1.13
 
-require github.com/gorilla/mux v1.7.4
+require (
+	github.com/gorilla/mux v1.7.4
+	go.mongodb.org/mongo-driver v1.3.2
+)
--- main.go
+++ main.go
@@ -1,8 +1,14 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
 	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"log"
 	"net/http"
 )
 
@@ -28,10 +34,52 @@ type Assignee struct {
 
 func main()  {
 	router:=mux.NewRouter()
+	ConnectDB()
 	router.HandleFunc("/",helloFunc).Methods("GET")
-	router.HandleFunc("candidate/create",createCandidate).Methods("POST")
+	router.HandleFunc("/candidate/create",createCandidate).Methods("POST")
+	router.HandleFunc("/candidates",getCandidates).Methods("GET")
 	http.ListenAndServe(":8080",router)
 
+}
+
+func getCandidates(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var candidates []Candidate
+	//Connection mongoDB with helper class
+	collection := ConnectDB()
+	fmt.Println(collection)
+	// bson.M{},  we passed empty filter. So we want to get all data.
+	cur, err := collection.Find(context.TODO(), bson.M{})
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// Close the cursor once finished
+	/*A defer statement defers the execution of a function until the surrounding function returns.
+	simply, run cur.Close() process but after cur.Next() finished.*/
+	defer cur.Close(context.TODO())
+
+	for cur.Next(context.TODO()) {
+
+		// create a value into which the single document can be decoded
+		var candidate Candidate
+		// & character returns the memory address of the following variable.
+		err := cur.Decode(&candidate) // decode similar to deserialize process.
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// add item our array
+		candidates = append(candidates, candidate)
+	}
+
+	if err := cur.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	json.NewEncoder(w).Encode(candidates) // encode similar to serialize process.
 }
 
 func createCandidate(w http.ResponseWriter, r *http.Request) {
@@ -40,4 +88,24 @@ func createCandidate(w http.ResponseWriter, r *http.Request) {
 
 func helloFunc(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w,"Hello Otsimo\n")
+}
+// ConnectDB : This is helper function to connect mongoDB
+// If you want to export your function. You must to start upper case function name. Otherwise you won't see your function when you import that on other class.
+func ConnectDB() *mongo.Collection {
+
+	// Set client options
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+
+	// Connect to MongoDB
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected to MongoDB!")
+
+	collection := client.Database("Otsimo").Collection("Candidates")
+
+	return collection
 }
